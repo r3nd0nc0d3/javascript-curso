7793bcf3c4f614e959c5964be868d0182e83cb68
Jesus Mateo Rendon Avila <rendoncode@gmail.com>
2024-01-28 21:55:29 -0600

    Operadores y condicionales

--- compuestos.js
+++ compuestos.js
@@ -41,7 +41,57 @@ console.log(resultado2);
 //Esta es una Funcion Expresada por lo que no podemos invocarla antes de su declaracion                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 console.warn('ARRAYS');
 
-const miArreglo = ['hola', 'mundo',12,13,14];
+const miArreglo = ['hola', 'mundo', 12, ['arreglo',['dentro','de otro']]];
+console.log(miArreglo.indexOf(12));
+console.log(miArreglo[1]);
+console.log(miArreglo[1][2]);
+console.log(miArreglo[3][1][0]);
 miArreglo.pop();
 miArreglo.push('ultimo elemento');
-console.log(miArreglo.indexOf(4));
+console.log(miArreglo);
+
+const c = Array.of('X','Y','Z',9,8,7);
+console.log(c);
+
+const d = Array(50).fill('repeat');
+console.log(d);
+
+const colores = ['rojo', 'azul', 'verde'];
+colores.push('negro');
+console.log(colores);
+colores.pop();
+console.log(colores);
+
+colores.forEach(function(el){
+    console.log(`<li>${el}</il>`);
+})
+
+//Objetos  
+//Dentro de los objetos las variables son atributos o propiedades y las funciones son metodos                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+console.warn('OBJETOS');
+
+const mateo = {
+    nombre: 'Mateo',
+    apellido: 'Rendon',
+    edad: 30,
+    pasatiempo: ['gym', 'programming', 'cine'],
+    contacto:{
+        email:'rendoncode@gmail.com',
+        movil:'5518176360'
+    },
+    saludar:function(){
+        console.log(`hola me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} anios`);
+    }
+
+}
+
+console.log(mateo);
+// Accrder a valores
+console.log(mateo.nombre);
+console.log(mateo.pasatiempo[1]);
+mateo.saludar()
+console.log(Object.keys(mateo));
+console.log(Object.values(mateo));
+console.log(mateo.hasOwnProperty('nombre'));
+
+
